b71ed1156614075f728b92cdcc5e1d80f04529bb
diaoyong3777 <2151156420@qq.com>
2025-10-20 19:32:31 +0800

    finish

--- follow_train.py
+++ follow_train.py
@@ -1,6 +1,6 @@
 from base import *
 
-# # 将所有参数解冻,注意！！！！！！这里会显示只训练输出层，但其实都训练了
+# # 将所有参数解冻, 注意！！！！！！这里会显示只训练输出层，但其实都训练了
 # 【或者在配置中设置False即可,为了方便可以直接这样免得改。如果要只训练输出层，把下面注释掉就行】
 for param in model_ft.parameters():
     param.requires_grad = True
