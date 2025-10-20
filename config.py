ba3ae009c1756be2b90cb8ccdeaa22f6e493c595
diaoyong3777 <2151156420@qq.com>
2025-10-20 18:24:10 +0800

    finish

--- config.py
+++ config.py
@@ -3,6 +3,7 @@ import torch
 
 class TrainingConfig:
     model_name = 'resnet50'# 【越大越好】
+    filename = 'resnet50.pt'  # 权重保存位置
     feature_extract = True # 【对应2次训练的思路可以先True，再False】
 
     device = torch.device("cuda:0" if torch.cuda.is_available() else "cpu") #gpu
@@ -10,14 +11,13 @@ class TrainingConfig:
     batch_size = 128
 
     # 数据增强参数,大一点会更强
-    Resize = 128  # 统一大小
-    CenterCrop = 100  # 裁剪大小
+    Resize = 96  # 统一大小
+    CenterCrop = 64  # 裁剪大小
 
-    learning_rate = 0.01
-    step_size = 7 # 每5步，衰减到原来的0.2
+    learning_rate = 0.001
+    step_size = 2 # 每5步，衰减到原来的0.2
     gamma = 0.1
-    num_epochs = 30 # 次数
-    filename = 'pt/resnet50.pt' # 权重保存位置
+    num_epochs = 10 # 次数
 
 
 training_config = TrainingConfig()
